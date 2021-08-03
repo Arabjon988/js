777a21def9df8f80a81052959414c157a098dbeb
Arabjon9899 <arabkhon3133@gmail.com>
2021-08-03 18:47:40 +0500

    bla bla

---
+++ index.js
@@ -0,0 +1,46 @@
+const number = Math.floor(Math.random()*100)
+const number1 = Math.floor(Math.random()*1000)
+
+if(number< 99){
+    console.log(number+2)
+    alert("59")
+}
+
+ else if (number> 59){
+    console.log(number+ 1)
+    alert("59dan katta")
+}
+
+else {
+alert("10dan kichik")
+
+}
+
+const number3 = [1,5,6,8,45,25,65,13,45,56,23,2021,2302,545]
+
+for(let i=0; i<number3.length; i++){
+    if(i/2 || i/3) {
+        console.log('tubson emas')
+    }
+    else if (i/4 || i/5){
+
+    }
+}
+
+
+// if(a/a && a/1){
+//     console.log("tubsonlar jamlanmasi")
+// }
+
+// const IbrohimTeacher= [1,5,6,8,25,65,13,45,56,23,2021,2302,545]
+// const AbbocTeacher= false
+// {IbrohimTeacher? alert("hfkjhgkjfgkjsfjk") : alert("2g12h13g1h31g3df")}
+// if(
+//     IbrohimTeacher&&AbbocTeacher
+// ){
+//     alert("hfkjhgkjfgkjsfjk")
+// }
+
+// else if(IbrohimTeacher===true  || AbbosTeacher===false){
+//     alert("2g12h13g1h31g3df")
+// }
